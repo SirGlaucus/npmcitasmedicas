1ec3f08188f607d0c8a00c89c379d1fee7268f3e
SirGlaucus <glaucusjoseph@gmail.com>
2022-05-05 21:42:42 -0400

    Falta terminar el readme

--- index.js
+++ index.js
@@ -6,25 +6,38 @@ const _ = require('lodash')
 const chalk = require('chalk')
 
 http
-    .createServer(function (req, res) {
-        if(req.url.includes('/buscar')){
+    .createServer((req, res) => {
+        if (req.url.includes('/buscar')) {
+            // 4. Por cada consulta realizada al servidor, se debe devolver al cliente una lista con los
+            // datos de todos los usuarios registrados usando Lodash para recorrer el arreglo de
+            // usuarios.
+
             axios
-            .get('https://randomuser.me/api/?results=7')
-            .then((data) => {
-                const datosUsuarios = data.data.results
-                let contador = 1;
-                _.forEach(datosUsuarios, (e) => {
-                    console.log(chalk.blue.bgWhite.bold(`${contador}. Nombre: ${e.name.first} Apellido: ${e.name.last} - ID: ${uuidv4().slice(0, 6)} Timestamp: ${fecha().format('MMMM Do YYYY, h:mm:ss a')}`))
-                    res.write(`${contador}. Nombre: ${e.name.first} Apellido: ${e.name.last} - ID: ${uuidv4().slice(0, 6)} Timestamp: ${fecha().format('MMMM Do YYYY, h:mm:ss a')}\n`)
-                    console.log('------------------------------------------------------')
-                    contador++
+                .get('https://randomuser.me/api/?results=7') // 1. El registro de los usuarios debe hacerse con la API Random User usando axios para consultar la data.
+                .then((data) => {
+                    const datosUsuarios = data.data.results
+                    let contador = 1
+                    // 2. Cada usuario registrado debe tener un campo id único generado por el paquete UUID.
+                    // 3. Cada usuario debe tener un campo timestamp almacenando la fecha de registro obtenida por medio del paquete Moment.
+
+                    _.forEach(datosUsuarios, (e) => {
+                        // 5. En cada consulta también se debe imprimir por la consola del servidor la misma lista
+                        // de usuarios pero con fondo blanco y color de texto azul usando el paquete Chalk.
+                        console.log(chalk.blue.bgWhite.bold(`${contador}. Nombre: ${e.name.first} Apellido: ${e.name.last} - ID: ${uuidv4().slice(0, 6)} Timestamp: ${fecha().format('MMMM Do YYYY, h:mm:ss a')}`))
+
+                        res.write(`${contador}. Nombre: ${e.name.first} Apellido: ${e.name.last} - ID: ${uuidv4().slice(0, 6)} Timestamp: ${fecha().format('MMMM Do YYYY, h:mm:ss a')}\n`)
+
+                        contador++ // El contador es utlizado para listar con un numero cada impresion, tanto en el console.log como en el documento con el res.write
+                    })
+                    res.end()
                 })
-                res.end()
-            })
-            .catch((e) => {
-                console.log(e)
-            })
+                .catch((e) => {
+                    console.log(e)
+                })
         }
     })
-    .listen(8080, () => console.log('Escuchando el puerto 8080')) // Paso 4
+    .listen(8080, () => console.log('Escuchando el puerto 8080'))
 
+// 6. El servidor debe ser levantado con el comando Nodemon
+// NOTA: Nodemon fue instalado de manera global al crear este proyecto
+// NOTA: Se debe usar la linea: npm install -g nodemon
